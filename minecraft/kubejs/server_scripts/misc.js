f98fbcdfc8aecf4cb3db8eb1ff3979eb427d203a
knight4him <josiah@JsschoolLaptop.>
2026-06-04 15:18:03 -0600

    update

--- minecraft/kubejs/server_scripts/misc.js
+++ minecraft/kubejs/server_scripts/misc.js
@@ -3,5 +3,6 @@ ServerEvents.recipes(event => {
   event.remove({output:'itoneo:katometer'}),
   event.remove({output:'itoneo:telosmeter'}),
   event.remove({output:'itoneo:multi_katometer'}),
-  event.remove({output:'itoneo:viator'})
+  event.remove({output:'itoneo:viator'}),
+  event.replaceOutput({},'minecraft:ender_pearl','minecraft:air')
 })
